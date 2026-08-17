84ceffa8594f456fa00796992ff46099813b5bec
Natalia Kaczor <Natalia.Kaczor@Student.HTW-Berlin.de>
2026-08-17 13:50:13 +0200

    navbar + routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Lebensmittelliste } from './lebensmittelliste/lebensmittelliste';
+import { Lebensmittelansicht } from './lebensmittelansicht/lebensmittelansicht';
+import { LebensmittelForm } from './lebensmittel-form/lebensmittel-form';
+import { BabyProfil } from './baby-profil/baby-profil';
+import { Ernaehrungstagebuch } from './ernaehrungstagebuch/ernaehrungstagebuch';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home},
+    { path: 'lebensmittelliste', component: Lebensmittelliste},
+    { path: 'lebensmittelansicht', component: Lebensmittelansicht},
+    { path: 'lebensmittelform', component: LebensmittelForm},
+    { path: 'baby-profil', component: BabyProfil},
+    { path: 'ernaehrungstagebuch', component: Ernaehrungstagebuch},
+];
