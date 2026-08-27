0ce893b7489133f2a3e3658a9a5dc80116c065df
Natalia Kaczor <Natalia.Kaczor@Student.HTW-Berlin.de>
2026-08-27 18:34:19 +0200

    Interface Lebensmittel erstellt

---
+++ src/interfaces/lebensmittel.ts
@@ -0,0 +1,9 @@
+export interface Lebensmittel {
+  _id?: string; // wird automatsich vom DB erstellt; wird später zur Zählung der LB, die ein Baby bereits probiert hat 
+  name: string;
+  kategorie: "Obst" | "Gemüse" | "Getreide" | "Fleisch" | "Fisch" | "Milchprodukte" | "andere Kategorie";
+  altersempfehlung: string; 
+  allergen: "keines" | "Milch" | "Ei" | "Nüsse" | "Fisch" | "Gluten";
+  beschreibung?: string;
+  bild: string; 
+}
