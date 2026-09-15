20c7cffda14ab9e9c5b475ab83d721a65cb83cf1
Natalia Kaczor <Natalia.Kaczor@Student.HTW-Berlin.de>
2026-09-15 14:08:07 +0200

    getOne() Lebensmittel für lebensmittelansicht umgesetzt

--- src/app/lebensmittelansicht/lebensmittelansicht.ts
+++ src/app/lebensmittelansicht/lebensmittelansicht.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Backend } from '../shared/backend';
+import { Lebensmittel } from '../../interfaces/lebensmittel';
+import { RouterLink, ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-lebensmittelansicht',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './lebensmittelansicht.html',
   styleUrl: './lebensmittelansicht.css',
 })
-export class Lebensmittelansicht {}
+export class Lebensmittelansicht implements OnInit {
+
+  lebensmittel!: Lebensmittel;
+
+  constructor(
+    private backend: Backend,
+    private route: ActivatedRoute,
+    private cdr:ChangeDetectorRef
+  ) { }
+
+  async ngOnInit() {
+
+    console.log('1. ngOnInit gestartet');
+
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+
+      this.lebensmittel = await this.backend.getOne(id);
+      console.log('folgendes Lebensmittel wurde geklickt:', this.lebensmittel);
+
+    }
+    this.cdr.detectChanges();
+
+  }
+}
