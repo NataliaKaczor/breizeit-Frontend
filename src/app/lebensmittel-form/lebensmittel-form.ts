c3ceb6303f7d6e29e8beec390d16e0014b5156bd
Natalia Kaczor <Natalia.Kaczor@Student.HTW-Berlin.de>
2026-09-17 14:51:51 +0200

    onSubmit() Lebensmittel erstellt

--- src/app/lebensmittel-form/lebensmittel-form.ts
+++ src/app/lebensmittel-form/lebensmittel-form.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Lebensmittel } from '../../interfaces/lebensmittel';
+import { Backend } from '../shared/backend';
 @Component({
   selector: 'app-lebensmittel-form',
   imports: [],
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './lebensmittel-form.css',
 })
 export class LebensmittelForm {
+
+  lebensmittel: Lebensmittel = {
+      name: '',
+      kategorie: 'Obst',
+      altersempfehlung: '',
+      allergen: 'keines',
+      beschreibung: '',
+      bild: ''
+    };
+
+  constructor( private backend: Backend){} 
   
+  onSubmit() {
+    this.backend.create(this.lebensmittel).then(() => {
+      console.log('Neuer Lebensmittel wurde hinzugefügt', this.lebensmittel);
+     
+    });
+  }
 }
